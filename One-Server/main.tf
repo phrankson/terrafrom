4531a5c62d613911aeb38bf5d1a6d75cbf1e8cc1
phrankson <phrvnk@gmail.com>
2024-11-12 08:07:49 +0100

    one server

---
+++ One-Server/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-07ba1d57e5f47e6c7"
+  lifecycle {
+    create_before_destroy = true
+  }
+
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
